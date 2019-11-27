3129dc70fd266686a69ce65fdb6b775edd707fc2
flyinactor91 <michael@mdupont.com>
2019-11-26 20:29:46 -0500

    Push bug

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 import sys
 
 def test_python_version():
-    assert sys.version_info.major == 3 and sys.version_info.minor >= 6
+    assert sys.version_info.major == 3 and sys.version_info.minor >= 9
